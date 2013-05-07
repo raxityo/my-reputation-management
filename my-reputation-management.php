7816ded845cb426b627b9affd2145b9c728b58c6
Rakshit Majithiya <rakshit@motioneleven.com>
2013-05-06 23:11:47 -0400

    Add Post Type registration code

--- my-reputation-management.php
+++ my-reputation-management.php
@@ -1,12 +1,13 @@
 <?php
 /*
 Plugin Name: My Reputation Management
+Description: A reputation management plugin developed by rax
 Plugin URI: http://thetechtime.com/
 Version: 0.1
 Author: Rakshit Majithiya (rax)
 Author URI: http://geekrax.com/
 */
-
+include_once 'register-posttype.php';
 
 
 ?>
---
+++ register-posttype.php
@@ -0,0 +1,24 @@
+<?php
+function register_client_post_type() {
+
+   /**
+    * Register a custom post type
+    *
+    * @see register_post_type for full list of options for register_post_type
+    * @see add_post_type_support for full descriptions of 'supports' options
+    * @see get_post_type_capabilities for full list of available fine grained capabilities that are supported
+    */
+    register_post_type( 'rm_client',
+		array(
+			'labels' => array(
+				'name' => __( 'Clients' ),
+				'singular_name' => __( 'Client' )
+			),
+		'public' => true,
+		'has_archive' => true,
+		'rewrite' => array('slug' => 'products'
+		)
+	);
+}
+add_action( 'init', 'register_client_post_type' );
+?>
